972156019196814a3a269a6899018a060ff92d77
onuroziskender <oozisk01@student.bbk.ac.uk>
2024-11-15 16:28:37 +0000

    Header and Footer Components created. Home Page Created.

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function Header () {
+    return(
+        <React.Fragment>
+            <nav>
+                <div> Home </div>
+            </nav>
+        </React.Fragment>
+    );
+
+}
